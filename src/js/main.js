810156d50633960e53c42545a20e3c4589fdcac6
aditi-s-4465 <aditi12@illinois.edu>
2025-09-26 22:44:59 -0500

    add modal

--- src/js/main.js
+++ src/js/main.js
@@ -52,4 +52,23 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
 }
 
+var modal = document.getElementById("mode");
+
+var btn = document.getElementById("mb");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
